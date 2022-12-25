fe49782a1df68bb3a464d4512679be6ac2337677
ahmed magdy <ahmedmagdy2016@gmail.com>
2022-12-25 03:51:20 +0200

    add api property for dto, add api tag & response annotation

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,16 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CustomersService } from './customers.service';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 
+@ApiTags('customers')
 @Controller('customers')
 export class CustomersController {
   constructor(private readonly customersService: CustomersService) {}
 
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+  })
   @Post()
   create(@Body() createCustomerDto: CreateCustomerDto) {
     return this.customersService.create(createCustomerDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'The record found.',
+  })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.customersService.findOne(id);
--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCustomerDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 }
